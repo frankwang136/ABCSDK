448d0230104b15974e71f7c145476bcc8c34b402
DW-FrankWang <frank.wang@datawise.com.hk>
2024-02-27 17:06:57 +0800

    test

---
+++ ABC.xcframework/ios-arm64/ABC.framework/Headers/ABC.h
@@ -0,0 +1,18 @@
+//
+//  ABC.h
+//  ABC
+//
+//  Created by 王志忠 on 2024/2/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ABC.
+FOUNDATION_EXPORT double ABCVersionNumber;
+
+//! Project version string for ABC.
+FOUNDATION_EXPORT const unsigned char ABCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ABC/PublicHeader.h>
+
+#import <ABC/Test.h>
---
+++ ABC.xcframework/ios-arm64/ABC.framework/Headers/Test.h
@@ -0,0 +1,16 @@
+//
+//  Test.h
+//  ABC
+//
+//  Created by 王志忠 on 2024/2/27.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : NSObject
+- (void)run;
+@end
+
+NS_ASSUME_NONNULL_END
